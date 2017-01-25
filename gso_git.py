978e100168017a7d7a4d55348f00b82320f53abf
FannarHrafnH <fannarha404@nemi.tskoli.is>
2017-01-25 13:51:44 +0000

    Allt búið

--- gso_git.py
+++ gso_git.py
@@ -1,8 +1,19 @@
 #Fannar Hrafn Haraldsson
 #25.1.17
 #Git verkefni
+#spyr um nafn
 nafn=input("Hvað heitir skráin?: ")
+#bæti við .txt
 skraheiti=nafn+".txt"
+#opna skrá með w+ til að búa hana til
 skra = open(skraheiti,'w+')
-skra.write('virkar\n')
+#skrifa í skrá \n gerir nýja línu
+skra.write('Þetta virkar\n')
+#loka skrá
+skra.close()
+#opna skrá aftur
+skra = open(skraheiti,'a')
+#skrifa aftur í skrá, þessi kóði lítur ekkert sérstaklega vel út
+skra.write('Alveg heilar\nþrjár\nlínur')
+#loka skrá
 skra.close()
